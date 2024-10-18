a2e2acc186a4a1623435d168e7a882963e6a7be7
Henrique Torres <henriquetorrescampos@gmail.com>
2024-10-18 19:26:00 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Slider from '@react-native-community/slider';
+
+export default class App extends Components{
+    constructor(props){
+        super(props);
+        this.state = { 
+            valor: 50
+        };
+    }
+
+    render(){
+        return(
+            <View style={styles.container}>
+
+                <Slider 
+                    minimumValue={0}
+                    maximumValue={100}
+                    onValueChange={ (ValorSelecionado)=> this.setState({valor:ValorSelecionado}) }
+                    value={this.state.valor}
+                    minimumTrackTintColor="#00FF00"
+                    maximumTrackTintColor={"#FF0000"}
+                />
+
+                <Text style={{TextAlign: 'center', fontSize: 30}}>
+                   Você tem: {this.state.valor.toFixed(1)} kg
+                </Text>
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container:{
+        flex: 1,
+    }
 });
